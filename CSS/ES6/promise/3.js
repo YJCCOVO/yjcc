4f1adb2b8bd0d9c0f9b639acc5a42ca632c3464c
CC. <yangjunxi18@foxmail.com>
2023-11-14 21:31:55 +0800

    外包

--- CSS/ES6/promise/3.js
+++ CSS/ES6/promise/3.js
@@ -5,7 +5,6 @@ function a() {
             resolve('yes')
         },1000)
     })
-    
 }
 
 function b() {
@@ -21,9 +20,9 @@ function c() {
     console.log('c');
 }
 
-// Promise.all([a(),b()]).then(c)
+Promise.all([a(),b()]).then(c)
 // all 必须要接受到的所有的promise 状态都为resolve，then才会调用
 
-Promise.race([a(),b()])
-.then(c)
+// Promise.race([a(),b()])
+// .then(c)
 // race a() 或 b() 哪个更快c就在哪个后面执行
